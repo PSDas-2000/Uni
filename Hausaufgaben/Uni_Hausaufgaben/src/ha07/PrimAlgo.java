a6517932f6a723a7cf2705a9254b910d71afd312
Paramvir Das <paramvir2307@gmail.com>
2021-05-24 15:53:01 +0200

    Alternate method for Algorithm of Prim added as comments

--- Hausaufgaben/Uni_Hausaufgaben/src/ha07/PrimAlgo.java
+++ Hausaufgaben/Uni_Hausaufgaben/src/ha07/PrimAlgo.java
@@ -26,7 +26,7 @@ public class PrimAlgo {
 		int cost = 0;
 		boolean[] check = new boolean[edges.length];
 		int start[] = {3};
-		System.out.println("Waehle 2 als Wurzel");
+		System.out.println(String.format("Waehle %d als Wurzel", start[0]));
 		check[start[0] - 1] = true;
 		int minCostElem = getMinCostElem(edges, check, start);
 		while (minCostElem != -1) {
@@ -38,9 +38,10 @@ public class PrimAlgo {
 		return cost;
 	}
 
-	// Helping method that returns the edge which takes the least cost
+	// Helping method that returns the *edge* which takes the least cost
 	// to travel from fromNode. This edge should also have not been used
-	// for the minimal spanning tree before.
+	// for the minimal spanning tree before. The method also adjusts the
+	// start value accordingly
 	private static int getMinCostElem(int[][] edges, boolean[] check, int[] start) {
 		int minCostElem = -1;
 		int minCost = 0;
@@ -50,11 +51,7 @@ public class PrimAlgo {
 				fromNode = j+1;
 				for (int i = 0; i < edges.length; i++) {
 					if (!check[i]) {
-						if (minCost == 0 && edges[fromNode - 1][i]!=0) {
-							minCost = edges[fromNode - 1][i];
-							minCostElem = i + 1;
-							start[0] = fromNode;
-						} else if (edges[fromNode - 1][i] != 0 && edges[fromNode - 1][i] < minCost) {
+						if ((minCost == 0 || edges[fromNode - 1][i] < minCost) && edges[fromNode - 1][i]!=0) {
 							minCost = edges[fromNode - 1][i];
 							minCostElem = i + 1;
 							start[0] = fromNode;
@@ -65,4 +62,25 @@ public class PrimAlgo {
 		}
 		return minCostElem;
 	}
+	
+//	private static int getMinCostElem1(int[][] edges, ArrayList<Integer> check, int[] start) {
+//		int minCostElem = -1;
+//		int minCost = 0;
+//		int fromNode = 0;
+//		for (int j = 0; j < edges.length; j++) {
+//			if (check.contains(j)) {
+//				fromNode = j+1;
+//				for (int i = 0; i < edges.length; i++) {
+//					if (!check.contains(start)) {
+//						if ((minCost == 0 || edges[fromNode - 1][i] < minCost) && edges[fromNode - 1][i]!=0) {
+//							minCost = edges[fromNode - 1][i];
+//							minCostElem = i + 1;
+//							start[0] = fromNode;
+//						}
+//					}
+//				}
+//			}
+//		}
+//		return minCostElem;
+//	}
 }
